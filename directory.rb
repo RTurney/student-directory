d08112264f271e2b59fa6eebd29ae6d759acaf26
RTurney <r.turney93@gmail.com>
2020-12-15 21:04:14 +0000

    Reorderd the functions and changed larger functions into smaller ones

--- directory.rb
+++ directory.rb
@@ -1,54 +1,72 @@
+# function for interactive menu
 def interactive_menu
-   students = [] 
+   @students = [] 
    loop do 
-       puts "Please pick one of the following options"
-       puts "1. Input the students"
-       puts "2. Show the students"
-       puts "9. Exit the program"
-       user_selection = gets.chomp
-       
-       case user_selection
-        when "1"
-           students = input_students(students)
-        when "2"
-            header
-            print(students)
-            footer(students)
-        when "9"
-            exit 
-        else 
-            puts "I do not recognise this input, please choose again"
-       end
+       print_menu
+       process(gets.chomp)
     end
 end
 
+# print menu function
+def print_menu
+    puts "Please pick one of the following options"
+       puts "1. Input the students"
+       puts "2. Show the students"
+       puts "9. Exit the program" 
+end
 
-def input_students(students)
+#function for defining each selection in the menu
+def process(selection)
+    case selection
+     when "1"
+       @students = input_students
+    when "2"
+        showing_students
+    when "9"
+        exit 
+    else 
+        puts "I do not recognise this input, please choose again"
+    end
+end
+
+# function for inputting students to array
+def input_students
    puts "please put the names of the students"
    puts "To finish just hit the return twice"
    name = gets.chomp
    while !name.empty? do
-       students << {name: name, cohort: :november}
-       puts "Now we have #{students.count} students"
+       @students << {name: name, cohort: :november}
+       puts "Now we have #{@students.count} students"
        name = gets.chomp
    end
-    students
+    @students
 end    
 
+# function fot printing the header
 def header
     puts "The students of Villians academy"
     puts '--------------------------------'
 end
 
-def print(students)
-    students.each.with_index do |student, index|
+#function for printing student array
+def print_students_list
+    @students.each.with_index do |student, index|
         puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
 end
 
-def footer(students)
-    puts "Overall, we have #{students.count} great students"
+# function for printing the footer
+def footer
+    puts "Overall, we have #{@students.count} great students"
     puts "--------------------------------------------------"
 end
 
+# function responsible for the printing students aesthetic
+def showing_students
+    header
+    print_students_list
+    footer
+end
+
+#call methods 
 interactive_menu
