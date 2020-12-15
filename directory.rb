2fd97affeed0612a3e034171d01be6b159dae43c
EC2 Default User <ec2-user@ip-172-31-33-87.us-east-2.compute.internal>
2020-12-15 15:44:19 +0000

    updated directory with array

--- directory.rb
+++ directory.rb
@@ -1,16 +1,31 @@
 student_count = 11
 
+students = [ 
+            "Dr Hannibal Lecter", 
+            "Darth Vader", 
+            "Nurse Ratchet", 
+            "Michael Corleone", 
+            "Alex DeLarge", 
+            "The Wicked Witch of the West", 
+            "Terminator", 
+            "Freddy Krueger", 
+            "The Joker", 
+            "Joffrey Baratheon", 
+            "Norman Bates"
+            ]
+
 puts "The students of Villians academy"
 puts '--------------------------------'
-puts "Dr Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratchet"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "overall we have #{student_count} great students"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts students[10]
+
+puts "overall we have #{student_count} great students"
